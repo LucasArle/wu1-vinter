c43cc5ece729c1c7e570531b22b7b3592140b6ea
LucasArle <lucas.arletun@elev.ga.ntig.se>
2025-12-09 18:43:39 +0100

    vatten

--- js/Particle.js
+++ js/Particle.js
@@ -1,11 +1,11 @@
 class Particle {
   constructor(x, y, color) {
-    this.x = x;
-    this.y = y;
-    this.dy = (1 + Math.random() * 3) * speed / 5;
-    this.dx = (-1 + Math.random() * 2) * speed / 5;
-    this.color = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${Math.random()})`;
-    this.size = 2 + Math.floor(Math.random() * 2);
+    this.x = 400;
+    this.y = 0;
+    this.dy = (1 + Math.random() * 3) * speed / 1;
+    this.dx = (-1 + Math.random() * 2) * speed / 15;
+    this.color = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${0.2})`;
+    this.size = 2 + Math.floor(Math.random() * 8);
     this.toDelete = false;
   }
 
@@ -16,11 +16,58 @@ class Particle {
     ctx.fill();
   }
 
-  update() {
-    this.y += this.dy;
-    this.x += this.dx;
+  update(mouseX,mouseY,changedColor,deltaTime,mouseSX,mouseSY) {
+    
+    
+    if((this.y > mouseY -15)&&(this.y < mouseY +15)&&(this.x > mouseX -30)&&(this.x < mouseX +30)){
+      this.color = `rgba(${changedColor[0]}, ${changedColor[1]}, ${changedColor[2]}, ${0.2})`;;
+
+      // console.log(this.polarity(this.x-mouseX))
+      // debugger
+
+
+      // this.dx = this.polarity(this.x-mouseX) * ((100) - 100*(((this.x-mouseX)*this.polarity(this.x-mouseX))/50)) * deltaTime * 1000;
+      // this.y += this.dy * deltaTime;
+      // this.x += this.dx * deltaTime;
+      if ((this.x > mouseX -5)&&(this.x < mouseX +5)){
+        this.dx = (-1 + Math.random() * 2) * speed *1
+        this.dy = (1 + Math.random() * 3) * speed / 2;
+        this.y += this.dy * deltaTime;
+        this.x += this.dx * deltaTime;
+      }
+      else{
+        this.dx = (this.polarity(this.x-mouseX) * ((10) - 10*(((this.x-mouseX)*this.polarity(this.x-mouseX))/30)) * deltaTime * 1000) + ((-1 + Math.random() * 2) * speed / 1 * deltaTime) ;
+        this.dy = (1 + Math.random() * 3) * speed / 0.7;
+        this.y += this.dy * deltaTime;
+        this.x += this.dx * deltaTime;
+      }
+    
+      this.y += mouseSY * deltaTime * 30
+      this.x += (mouseSX*0.2) +( 1.3*mouseSX * Math.random()) * deltaTime * 30
+    }
+    else{
+      // this.dx =  (-1 + Math.random() * 2) * speed / 15 * deltaTime;
+      this.dx += (-this.dx / 50)
+      this.y += this.dy * deltaTime;
+      this.x += this.dx * deltaTime;
+    }
+
     if (this.y > canvas.height || this.x < 0 || this.x > canvas.width) {
       this.toDelete = true;
     }
   }
+
+  polarity(num){
+    if (num > 0){
+      return 1
+    }
+    else if(num<0){
+      return -1
+    }
+    else{
+      return 0
+    }
+      
+  }
+  
 }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 /* Om du vill ändra snöfärgen */
-const color = [255, 255, 255];
+const color = [200, 200, 255];
+const changedColor = [200, 200, 255]
+// const changedColor = [139, 207, 232]
+// const changedColor = [255, 255, 191]
 /* justera hur snabbt snön faller */
-const speed = 5;
+const speed = 200;
 
 /* Ändra här nedanför på egen risk */
+
+
+let mouseX = 0
+
+let mouseY = 0
+
+var mouseSX = 0
+var mouseSY = 0
+
+var preMouseX=0
+var preMouseY=0
+previousTimeStamp = 0
+
 
 const randomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -31,24 +47,44 @@ window.onscroll = () => {
   canvas.setAttribute("style", `top: ${window.scrollY}px`);
 };
 
+window.addEventListener("mousemove", (event) => {
+  // console.log(event)
+  mouseX = event.offsetX
+  mouseY = event.offsetY
+
+  mouseSX = mouseX - preMouseX
+  mouseSY = mouseY - preMouseY
+  preMouseX = mouseX
+  preMouseY = mouseY
+
+  // console.log(mouseSX,mouseSY)
+
+
+})
+
+
 const spawnParticles = (amount) => {
   for (let i = 0; i < amount; i++) {
     particles.push(new Particle(randomInt(0, canvas.width), 0, color));
   }
 };
 
-const step = () => {
+const step = (timeStamp) => {
+  deltaTime = timeStamp-previousTimeStamp
+  deltaTime = deltaTime / 1000
+  // console.log(deltaTime)
+  previousTimeStamp = timeStamp
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   particles.forEach((particle) => {
     particle.draw();
-    particle.update();
+    particle.update(mouseX,mouseY,changedColor,deltaTime,mouseSX,mouseSY);
   });
 
   particles = particles.filter((particle) => !particle.toDelete);
 
-  if (particles.length < 400) {
-    spawnParticles(3);
+  if (true) {
+    spawnParticles(100);
   }
 
   window.requestAnimationFrame(step);
